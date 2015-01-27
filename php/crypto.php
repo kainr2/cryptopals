915247e7da458788022e3e93bfe016fd443d77a5
root <root@localhost.localdomain>
2015-01-26 19:36:47 -0800

    Update var names and comments

--- php/crypto.php
+++ php/crypto.php
@@ -95,9 +95,9 @@ class XorAlgo
 function s1c1()
 {
   $hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-  $bin_str = pack("H*", $hex_str);
-  echo "hex_str = " . $hex_str . "\n";
-  echo "base2  = " . $bin_str . "\n";
+  $bin_str = pack("H*", $hex_str);  // hex2bin()
+  echo "hex_str = $hex_str\n";
+  echo "base2   = $bin_str\n";
   echo "base64 = " . base64_encode($bin_str) . "\n";
 }
 
@@ -107,18 +107,18 @@ function s1c2()
   $str1 = "1c0111001f010100061a024b53535009181c";
   $str2 = "686974207468652062756c6c277320657965";
 
-  // Convert chars to 16bit hex (2 hex chars = 1 hex value)
-  $hex1 = pack("H*", $str1);
-  $hex2 = pack("H*", $str2);
-  $hex3 = $algo->xor_str_str($hex1, $hex2);
-  $str3 = implode(unpack("H*", $hex3));
+  // Convert hex chars to binary-format (2 hex chars: 0x00-0xFF)
+  $bin1 = pack("H*", $str1);
+  $bin2 = pack("H*", $str2);
+  $bin3 = $algo->xor_str_str($bin1, $bin2);
+  $str3 = implode(unpack("H*", $bin3));
 
   $length_str = strlen($str1);
-  $length_bin = strlen($hex1);
-  echo "length(str) = $length_str; length(hex) = $length_bin\n";
+  $length_bin = strlen($bin1);
+  echo "length(str) = $length_str; length(bin) = $length_bin\n";
   echo "str1 = $str1\n";
-  echo "str2 = $str2 = $hex2\n";
-  echo "str3 = $str3 = $hex3\n";
+  echo "str2 = $str2 = $bin2\n";
+  echo "str3 = $str3 = $bin3\n";
 
 }
 
@@ -126,27 +126,27 @@ function s1c2()
 function s1c3()
 {
   $algo = new XorAlgo();
-  $input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-  $hex_input = pack("H*", $input);
-  echo "input  = $input\n";
-  echo "packed = $hex_input\n";
+  $hex_str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+  $bin_str = pack("H*", $hex_str);
+  echo "hex_input = $hex_str\n";
+  echo "bin_input = $bin_str\n";
 
   // Try xor-ing from 0 to 255
   $stat = Array();
   foreach (range(0,255) as $key)
   {
-    // Convert decimal to hex value(string) and pack() to real hex-format
+    // Convert decimal to hex value(string) and pack() to binary-format
     // * http://stackoverflow.com/questions/5799399/php5-pack-is-broken-on-x84-64-env
-    $hex_key = pack("H*", dechex($key));
-    $hex_out = $algo->xor_str_int($hex_input, $hex_key);
-    $output  = implode(unpack("H*", $hex_out));
+    $bin_key = pack("H*", dechex($key));   // decbin()
+    $bin_out = $algo->xor_str_int($bin_str, $bin_key);
+    $hex_out = implode(unpack("H*", $bin_out));
 
     // var_dump($key);
+    // var_dump($bin_out);
     // var_dump($hex_out);
-    // var_dump($output);
 
     // Analyze the output to see if it's a legit string.
-    $item = $algo->analyze_xor($input, $key, $hex_out);
+    $item = $algo->analyze_xor($hex_str, $key, $bin_out);
 
     // Use the most simple analysis -- some vowels and spaces
     if ($item->nonchar_count == 0 && $item->vowel_count > 0
